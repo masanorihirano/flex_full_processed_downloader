e8d2456ea6c7f164be7cc9250a70d45a33d039bb
Masanori HIRANO <masa.hirano.1996@gmail.com>
2020-01-08 03:15:40 +0900

    finalize API starter

--- downloader.rb
+++ downloader.rb
@@ -1,25 +1,36 @@
 require 'googleauth'
-require 'google/apis/drive_v3'
 require 'googleauth/stores/file_token_store'
+require 'google/apis/drive_v3'
+
+TEAM_DRIVE_ID = "0AEJpBZKnwJBQUk9PVA"
 
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-CLIENT_SECRETS_PATH = File.expand_path("./.credential/config.json",__dir__)
-SCOPE = 'https://www.googleapis.com/auth/drive'
-TEAM_DRIVE_ID = "0AEJpBZKnwJBQUk9PVA"
-TOKEN_PATH = File.expand_path("./.credential/tokens.yaml",__dir__)
-client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
-token_store = Google::Auth::Stores::FileTokenStore.new(:file => TOKEN_PATH)
-authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-credentials = authorizer.get_credentials(client_id)
+token_store_file = File.expand_path('.credential/credentials.yaml', __dir__)
+scope = "https://www.googleapis.com/auth/drive"
+client_id = Google::Auth::ClientId.from_file(File.expand_path('.credential/client_secret.json', __dir__))
 
+token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_file)
+authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+
+credentials = authorizer.get_credentials('default')
 if credentials.nil?
-  url = authorizer.get_authorization_url(base_url: OOB_URI )
+  url = authorizer.get_authorization_url(base_url: OOB_URI)
   puts "Open #{url} in your browser and enter the resulting code:"
-  code = gets
-  credentials = authorizer.get_and_store_credentials_from_code(
-    user_id: user_id, code: code, base_url: OOB_URI)
+  code = $stdin.gets
+  credentials = authorizer.get_and_store_credentials_from_code(user_id: 'default', code: code, base_url: OOB_URI)
 end
+service = Google::Apis::DriveV3::DriveService.new
+service.authorization = credentials
+
+file_list = service.list_files(
+        corpora: 'teamDrive',
+        team_drive_id: TEAM_DRIVE_ID,
+	q: "name='#{ARGV[0]}.tar.xz'",
+	spaces: 'drive',
+        fields: 'files(id,trashed)',
+        include_team_drive_items: true,
+        supports_team_drives: true)
 
 
 
